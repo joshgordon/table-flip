3e26412dc0bfc58344711dc6c7b1fbcbb343f205
Josh Gordon <github@joshgordon.net>
2016-03-27 16:52:33 -0400

    Console.warn works now too.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+//console.error
 var e = console.error;
 console.error = function () {
   e("（╯°□°）╯︵ ┻━┻", ...arguments);
+};
+
+//console.warn
+var w = console.warn;
+console.warn = function () {
+  w("ಠ_ಠ", ...arguments);
 };
--- readme.md
+++ readme.md
@@ -3,6 +3,8 @@
 Prepends a table-flip emoji ( （╯°□°）╯︵ ┻━┻ ) before console errors. 
 That's all. 
 
+New in v1.0.1: prepends the look of disapproval (ಠ_ಠ) to warnings.
+
 ## Usage. 
 
 Import it. 
