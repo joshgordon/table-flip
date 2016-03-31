677bd4c5afd3c3f408170e3c97dc4e343c2fbcb0
Josh Gordon <github@joshgordon.net>
2016-03-30 21:44:42 -0400

    Works with older versions of javascript that don't support the spread operator.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //console.error
 var e = console.error;
 console.error = function () {
-  e("（╯°□°）╯︵ ┻━┻", ...arguments);
+  e.apply(undefined, ["（╯°□°）╯︵ ┻━┻"].concat(Array.prototype.slice.call(arguments)));
 };
 
 //console.warn
 var w = console.warn;
 console.warn = function () {
-  w("ಠ_ಠ", ...arguments);
+  e.apply(undefined, ["ಠ_ಠ"].concat(Array.prototype.slice.call(arguments)));
 };
